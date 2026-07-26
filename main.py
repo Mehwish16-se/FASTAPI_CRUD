0897b26c6cf664e732885c3574a6701fc03106a6
Mehwish16-se <mehwishrehman019@gmail.com>
2026-07-26 16:31:53 +0500

    satge 2 :read endpoints with 404

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {
-        "name": "Task API",
-        "version": "1.0",
-        "endpoints": ["/tasks"]
+tasks = [
+    {
+        "id": 1,
+        "title": "Study FastAPI",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": "practise fastapi",
+        "done": True
+    },
+    {
+        "id": 3,
+        "title": "complete assigmant",
+        "done": False
     }
+]
+@app.get("/tasks/{id}")
+@app.get("/tasks/{id}")
+def get_task(id: int):
 
-@app.get("/health")
-def health():
-    return {
-        "status": "ok"
-    }
+    for task in tasks:
+        if task["id"] == id:
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
